d23419ceb561dfc684a4a8cd8cac2e31ac2afab6
EvgeniyShipov <Shipov.foto@mail.ru>
2020-01-23 22:32:24 +0300

    сделал модуль под домашнюю работу

---
+++ spring-13/src/main/java/ru/otus/spring13/Spring13Application.java
@@ -0,0 +1,13 @@
+package ru.otus.spring13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13Application.class, args);
+    }
+
+}
