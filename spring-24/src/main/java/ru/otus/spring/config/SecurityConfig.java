78c6df61add3e6e5569ee8ff5c1669ec157d34f3
EvgeniyShipov <Shipov.foto@mail.ru>
2020-03-16 23:19:18 +0300

    spring-24 настроил базу для acl security

--- spring-24/src/main/java/ru/otus/spring/config/SecurityConfig.java
+++ spring-24/src/main/java/ru/otus/spring/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package ru.otus.spring.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,13 +14,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService libraryUserDetailsService;
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                     .authorizeRequests()
-                    .antMatchers("/**").hasAnyRole("USER")
+                    .antMatchers("/**").hasAnyRole("USER", "EDITOR", "ADMIN")
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -41,9 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.userDetailsService(libraryUserDetailsService)
-                .passwordEncoder(passwordEncoder());
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("password").roles("ADMIN")
+                .and()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("editor").password("password").roles("EDITOR");
     }
 }
