a8e4b7612ce19b6239c6a4d724215941fcc4bbe5
EvgeniyShipov <Shipov.foto@mail.ru>
2020-04-08 21:05:40 +0300

    spring-29, правка замечаний

--- spring-29/src/main/java/ru/otus/spring/config/AppHealthIndicator.java
+++ spring-29/src/main/java/ru/otus/spring/config/AppHealthIndicator.java
@@ -4,13 +4,19 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 import static org.springframework.boot.actuate.health.Health.up;
 
 @Component
 public class AppHealthIndicator implements HealthIndicator {
+
     @Override
     public Health health() {
-
-        return up().build();
+        if (new Random().nextBoolean()) {
+            return up().withDetail("message", "I'm ok!").build();
+        } else {
+            return up().withDetail("message", "Check again").build();
+        }
     }
 }
